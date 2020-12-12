f5173ed612ac3ca23eefc24641bf5c214ce602b0
Fernando Buccella <fer.buccella@gmail.com>
2020-12-12 15:34:39 -0300

    Complete exercise.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,59 @@
 'use strict'
 
-function foo() {
-  return 'bar'
+/**
+ * @summary Calculates the string mathematical expression.
+ *
+ * @param {string} str - The mathematical expression.
+ *
+ * @returns {number} The result of evaluating the given mathematical expression.
+ * */
+function calculate(str) {
+  if (/^\d+$/.test(str)) {
+    return parseInt(str, 10)
+  }
+
+  const terms = str.split(/(\+|-)/)
+
+  const processedTerms = terms.map((term) => {
+    if (['+', '-'].includes(term)) {
+      return term
+    }
+
+    if (/^\d+$/.test(term)) {
+      return parseInt(term, 10)
+    }
+
+    const splitTerm = term.split(/(\*|\/)/)
+    let termResult = parseInt(splitTerm[0], 10)
+
+    for (let i = 1; i < splitTerm.length; i += 2) {
+      const operator = splitTerm[i]
+      const operand = parseInt(splitTerm[i + 1], 10)
+
+      if (operator === '*') {
+        termResult *= operand
+      } else {
+        termResult /= operand
+      }
+    }
+
+    return termResult
+  })
+
+  let result = processedTerms[0]
+
+  for (let j = 1; j < processedTerms.length; j += 2) {
+    const operator = processedTerms[j]
+    const operand = processedTerms[j + 1]
+
+    if (operator === '+') {
+      result += operand
+    } else {
+      result -= operand
+    }
+  }
+
+  return result
 }
 
-module.exports = foo
+module.exports = calculate
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,26 @@
 'use strict'
 
 const { expect } = require('chai')
-const foo = require('../src')
+const calculate = require('../src')
 
 describe('Example', () => {
-  it('Should pass', () => {
-    expect(foo).to.be.a('function')
-    expect(foo.length).to.equal(0)
-    expect(foo()).to.equal('bar')
+  it('Should pass if expression is just a number', () => {
+    expect(calculate('88')).to.equal(88)
+  })
+
+  it('Should pass if expression only includes "+" and "-"', () => {
+    expect(calculate('100+23')).to.equal(123)
+    expect(calculate('5-10')).to.equal(-5)
+    expect(calculate('100+87-1508')).to.equal(-1321)
+  })
+
+  it('Should pass if expression only includes "*" and "/"', () => {
+    expect(calculate('8*5')).to.equal(40)
+    expect(calculate('121/11')).to.equal(11)
+    expect(calculate('4*10/2')).to.equal(20)
+  })
+
+  it('Should pass if expression contains a combination of "+", "-", "/" and "*"', () => {
+    expect(calculate('30*40+80/20-18')).to.equal(1186)
   })
 })
